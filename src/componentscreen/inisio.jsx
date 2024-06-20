9a739bb7115af590bd48e1630eb4a4caa7d7258e
JasonN13 <148019428+JasonN13@users.noreply.github.com>
2024-06-19 19:27:30 -0600

    diseño perron

--- src/componentscreen/inisio.jsx
+++ src/componentscreen/inisio.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { InisioSesionfrmHook } from '../Hooks/InisioSesionfrmHook';
-import '..//Style//Inisio.css'
+import '..//Style//login.css'
 
 export const Inisio = () => {
-    const { dataform, changehandler, submitHandler,Registrar } = InisioSesionfrmHook();
+    const { dataform, changehandler, submitHandler, Registrar } = InisioSesionfrmHook();
 
     return (
         <>
-            <div className='container' id='Registro'>
-                <form id='frm' onSubmit={submitHandler}>
-                    <h1>Inicio de sesión</h1>
+            <div className='container' id='registro-container'>
+                <form id='registro-form' onSubmit={submitHandler}>
+                    <h1 id='titulo'>Inicio de sesión</h1>
                     <div className="mt-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Nombre de Usuario</label>
-                        <input onChange={changehandler} type="text" className="form-control" name='nombre_usuario' />
+                        <label htmlFor="nombre-usuario" className="form-label">Nombre de Usuario</label>
+                        <input id='nombre-usuario' onChange={changehandler} type="text" className="form-control" name='nombre_usuario' />
                     </div>
                     <div className="mt-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input onChange={changehandler} type="password" className="form-control" name='contrasena' />
+                        <label htmlFor="contrasena" className="form-label">Contraseña</label>
+                        <input id='contrasena' onChange={changehandler} type="password" className="form-control" name='contrasena' />
                     </div>
-                    <div className='mt-3' id='btn'>
-                        <button type="submit" className="btn btn-primary w-100">Iniciar sesión</button>
+                    <div className='mt-3'>
+                        <button id='iniciar-sesion-btn' type="submit" className="btn btn-primary w-100">Iniciar sesión</button>
                     </div>
-                    <div className='mt-3' id='btn'>
-                        <button type="button" onClick={Registrar} className="btn btn-primary w-100">Registro</button>
+                    <div className='mt-3'>
+                        <button id='registro-btn' type="button" onClick={Registrar} className="btn btn-primary w-100">Registro</button>
                     </div>
                 </form>
             </div>
